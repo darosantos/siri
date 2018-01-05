94c63edf06fef6c33ba1fde7859a21726f743d09
Danilo Santos <danilo.santosrs@gmail.com>
2018-01-04 21:42:08 -0300

    Contém as definições para inclusão de classes. O arquivo faz a chamada da função spl_register_autoload, que recebe uma função callback, por sua vez esta função faz as inclusões de classes/interfaces/traits que estejam no diretório lib e conforme sua subpasta definida pelo namespace. A função faz registro de logs para caso ocorram erros nas possibilidades de inclusão da classe. Um log de erro é gravado caso o arquivo não exista, caso tenha tido problemas na inclusão ou caso o arquivo incluido não contenha a definição (classe/interfaces/trait) esperada. Os logs são gravados no diretório padrão definido no arquivo de configuração e é gravado um erro por linha com quatro colunas cada. Cada coluna no log carrega uma variedade de informações úteis para debug.

--- lib/base/setup.php
+++ lib/base/setup.php
@@ -1,17 +1,40 @@
 <?php
 spl_autoload_extensions ( '.php' );
-spl_autoload_register ( function ( $className ) {
-	$fileStream = ABSPATH . 'lib' . DIRECTORY_SEPARATOR . $className . '.php';
-	
-	if ( file_exists( $fileStream ) ){
-		require_once ( $fileStream );
-		$exists =  !class_exists( $className, false ) && !interface_exists( $className, false ) && !trait_exists( $className, false );
-		if ( $exists ) {
-			trigger_error( "Unable to load : $className", E_USER_WARNING );
-		}
-	}
-	else {
-		trigger_error( "Impossible load definition class", E_USER_WARNING );
-	}
+spl_autoload_register(function ($identityDefinition) {
+    $pathToFile = ABSPATH . DIRECTORY_SEPARATOR . 'lib' . $identityDefinition . '.php';
+
+    try {
+        if (file_exists($pathToFile)) {
+            $isIncluded = @include_once($pathToFile);
+            if (!$isIncluded) {
+                throw new Exception('Impossible load definition class in ');
+            }
+
+            $nameDefinition = getName($identityDefinition);
+            $hasClass = class_exists($nameDefinition, false);
+            $hasInterface = interface_exists($nameDefinition, false);
+            $hasTrait = trait_exists($nameDefinition, false);
+            if (!$hasClass || !$hasInterface || !$hasTrait) {
+                throw new Exception('Unable to load definition. Scope not defined.');
+            }
+        } else {
+            throw new Exception('Impossible load definition class.');
+        }
+    } catch (Exception $e) {
+        $identityErrorFrom = $_SERVER['REQUEST_METHOD'] . ' ' . $_SERVER['SERVER_PROTOCOL'] . ' ' . $_SERVER['REMOTE_ADDR'] . ':' . $_SERVER['REMOTE_PORT'];
+        $descriptionError = $e->getMessage() . ' - ' . $_SERVER['SCRIPT_FILENAME'] . ' in ' . $_SERVER['REQUEST_TIME'];
+        $errorMsg = date('d/m/Y h:i:s') . "\t" . $identityClass . "\t" . $identityErrorFrom . "t" . $descriptionError . "\n";
+        @error_log($errorMsg, 3, PATH_LOG . 'autoload.log');
+    }
+
 });
+
+/**
+ * Utility function
+ */
+function getName($identityDefinition)
+{
+    $identityClass = explode('\\', $identityDefinition);
+    return array_pop($identityClass);
+}
 ?>
