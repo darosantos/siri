9d43380772463227b866861578b2241f502f6efe
Danilo Santos <danilo.santosrs@gmail.com>
2018-01-04 21:30:14 -0300

    Alterada algumas constantes de escopo global utilizadas no projeto

--- index.php
+++ index.php
@@ -3,5 +3,14 @@ if( !isset( $_SESSION ) || session_status() == PHP_SESSION_DISABLED ){
 	session_start();
 }
 
+global $APP_ENGINE;
+
+/**
+ * Estrutura básica da aplicação
+ */
+$GLOBALS['APP_ENGINE'] = array('APP_NAME' => 'siri',
+    'VERIFIY_PHP_OPTIONS' => ''
+);
+
 include_once ('loader.php');
 ?>
